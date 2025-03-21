e515cc812477a414586a79c7a0deebaef33bb617
C9b3rD3vi1 <nicksonwe@gmail.com>
2025-03-22 01:20:37 +0300

    fibre framework and Go web application

---
+++ main.go
@@ -0,0 +1,15 @@
+package main
+
+import (
+    "fmt"
+    "github.com/gofiber/fiber/v2"
+
+)
+
+// fibre app main function
+func main() {
+    app := fiber.New()
+
+    fmt.Println("Server is running on port 3000")
+    app.Listen(":3000")
+}
