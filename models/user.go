11b717e24aa2b82d8ce082ca57dfd0e16380bf0d
C9b3rD3vi1 <nicksonwe@gmail.com>
2025-03-30 16:38:40 +0300

    post model created

--- models/user.go
+++ models/user.go
@@ -2,16 +2,26 @@ package models
 
 import "fmt"
 
-// User struct
+// User struct represents a user entity with personal and contact details.
 type User struct {
-	Name    string
-	Id      int
+	FullName    string
+	ID      int
 	Email   string
 	Phone   string
 	Address string
-	Status  bool
+	IsActive  bool
 }
 
 func UserCreate() {
+	user := User{
+		FullName: "Nana Kwame",
+		ID:   1,
+		Email:  "nana.kwame@gmail.com",
+        Phone:  "+2348123456789",
+        Address: "123 Main St, Anytown, USA",
+        IsActive: true,
+    }
+	
+	fmt.Println(user)
 	fmt.Println("User created")
 }
