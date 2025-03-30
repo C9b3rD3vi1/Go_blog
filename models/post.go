dc12a5f1211c6ec92bfba01db5e06c8de6f3c586
C9b3rD3vi1 <nicksonwe@gmail.com>
2025-03-31 00:06:33 +0300

    Go structures in web

--- models/post.go
+++ models/post.go
@@ -6,8 +6,24 @@ import "time"
 type Post struct {
 	ID        uint      `json:"id" gorm:"primaryKey"`
 	Title     string    `json:"title" gorm:"not null"`
+	Slug      string    `json:"slug" gorm:"not null"`
+	Image     string    `json:"image" gorm:"not null"`
 	Content   string    `json:"content" gorm:"not null"`
 	Author    string    `json:"author" gorm:"not null"`
+	Tags      string    `json:"tags" gorm:"not null"`
+	Category  Category  `json:"category" gorm:"foreignKey:CategoryID"`
 	CreatedAt time.Time `json:"created_at" gorm:"autoCreateTime"`
 	UpdatedAt time.Time `json:"updated_at" gorm:"autoUpdateTime"`
 }
+
+
+
+// category represents a blog post category in the database
+type Category struct {
+	ID        uint      `json:"id" gorm:"primaryKey"`
+	Name      string    `json:"name" gorm:"not null"`
+	CreatedAt time.Time `json:"created_at" gorm:"autoCreateTime"`
+	UpdatedAt time.Time `json:"updated_at" gorm:"autoUpdateTime"`
+}
+
+// 
