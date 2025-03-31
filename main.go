dcf16271bac42178604d59b1e0ec35a5062593b6
C9b3rD3vi1 <nicksonwe@gmail.com>
2025-04-01 00:41:10 +0300

    load template engine

--- main.go
+++ main.go
@@ -3,19 +3,23 @@ package main
 import (
     "fmt"
     "github.com/gofiber/fiber/v2"
-
+    "github.com/gofiber/template/html/v2"
+    
 
 )
 
-
 // fibre app main function
 func main() {
-    app := fiber.New()
+    // load template engine
+    engine := html.New("./views", ".html")
+    app := fiber.New(fiber.Config{Views: engine})
 
     fmt.Println("Server is running on port 3000")
 
+    // Route to render index.html
     app.Get("/", func(c *fiber.Ctx) error {
-        return c.SendString("Cyberlock Technologies!")
+        return c.Render("index", fiber.Map{
+            "Title": "Hello, World!, Welcome to the world of Go Fiber",})
     })
 
     // app listen on port 3000
