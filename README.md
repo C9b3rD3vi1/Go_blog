c13b6f5f6e0b4ebc9875697be1b77f21ec7d05e0
C9b3rD3vi1 <nicksonwe@gmail.com>
2025-07-15 15:14:07 +0300

    readme update with project web link

--- README.md
+++ README.md
@@ -1,5 +1,7 @@
 Go_blog
 
 
+![code sample](./static/images/main.png)
+
 ## Web link
 https://go-blog-3vwl.onrender.com/
--- models/user.go
+++ models/user.go
@@ -1,70 +1,66 @@
 package models
 
-import ("fmt"
+import (
+	"fmt"
+
 	"gorm.io/gorm"
 )
 
-
 // User struct represents a user entity with personal and contact details.
 type User struct {
-	FullName    string `gorm:"unique;not null"`
-	ID      int	`gorm:"primaryKey"`
-	Username  string `gorm:"unique;not null"`
-	Email     string `gorm:"unique;not null"`
-	Password  string `gorm:"required"`
+	FullName        string `gorm:"unique;not null"`
+	ID              int    `gorm:"primaryKey"`
+	Username        string `gorm:"unique;not null"`
+	Email           string `gorm:"unique;not null"`
+	Password        string `gorm:"required"`
 	PasswordConfirm string
-	Address string
-	IsActive  bool
+	Address         string
+	IsActive        bool
 
-	// admin 
+	// admin
 	IsAdmin bool `gorm:"default:false"`
 
 	gorm.Model
 }
-
 
 // Comment struct represents a comment entity with user and post details.
 type Comment struct {
 	ID      int `gorm:"primaryKey"`
 	Content string
 	// UserID is the foreign key for the user
-	UserID int 
-	User    User 
+	UserID int
+	User   User
 	// PostID is the foreign key for the post
 	PostID int
-	Post    Post
+	Post   Post
 
 	gorm.Model
 }
 
-
-
 // Create User function creates a new user entity.
-func CreateUser(ID int, fullName string,username string, email string, address string, isActive bool) User {
+func CreateUser(ID int, fullName string, username string, email string, address string, isActive bool) User {
 	user := User{
 		FullName: fullName,
-		ID:   ID,
+		ID:       ID,
 		Username: username,
-		Email:  email,
-		Address: address,
+		Email:    email,
+		Address:  address,
 		IsActive: isActive,
 	}
 
 	return user
 }
 
-
-
 // UserCreate function creates a new user entity.
 func UserCreate() {
 	user := User{
 		FullName: "Nana Kwame",
-		ID:   1,
-		Email:  "nana.kwame@gmail.com",
+		ID:       1,
+		Email:    "nana.kwame@gmail.com",
 		Username: "nana_kwame",
-        Address: "123 Main St, Anytown, USA",
-        IsActive: true,
-    }
+		Address:  "123 Main St, Anytown, USA",
+		IsActive: true,
+	}
 
 	fmt.Println(user)
 	fmt.Println("User created")
